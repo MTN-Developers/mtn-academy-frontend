334a46ae720528d8872a9bfa41477dc8d7ba69b2
Marina Safwat <66208355+marinaSwadee@users.noreply.github.com>
2025-01-24 17:20:46 +0200

    Enhance LearningStudyProgressCard layout for better responsiveness, adjust styles for image and button, and improve alignment of elements

--- app/components/common/LearningStudyProgressCard.tsx
+++ app/components/common/LearningStudyProgressCard.tsx
@@ -20,20 +20,20 @@ const LearningStudyProgressCard = ({ study }: { study: Study }) => {
     }) || study.mainCourses[0];
 
   return (
-    <div className="bg-white w-fit lg:min-w-[1005px] flex gap-4 items-stretch overflow-hidden shadow-sm rounded-3xl lg:py-[14px] lg:px-[24px]">
+    <div className="bg-white w-full max-w-[1005px] flex flex-col lg:flex-row gap-4 items-stretch overflow-hidden shadow-sm rounded-3xl p-4 lg:py-[14px] lg:px-[24px]">
       <Image
         src={study.image}
         alt="study image"
         width={200}
         height={140}
-        className="rounded-2xl shadow-2xl"
+        className="rounded-2xl shadow-2xl w-full lg:w-[200px] h-[140px] object-cover"
       />
       <div className="lg:w-[420px] flex flex-col justify-end lg:me-[32px]">
         <p className="text-sm block font-bold">{study.title}</p>
         <p className="text-[#07519C] lg:text-[32px] font-medium leading-[45px]">
           <TruncatedText text={courseToShow.title} />
         </p>
-        <div className="my-4 w-full">
+        <div className="lg:my-4 w-full">
           <Progress
             value={courseToShow.percentage}
             className={`w-[100%] bg-gray-200 ${
@@ -53,14 +53,14 @@ const LearningStudyProgressCard = ({ study }: { study: Study }) => {
           <Video />
           <p>EPOSIDE 2 PART FOUR</p>
         </div>
-        <div className="flex items-center justify-center gap-2    ">
+        <div className="flex items-center justify-start lg:justify-center gap-2    ">
           <p className="line-clamp-2 text-sm font-semibold text-nowrap text-[#303141]">
             <TruncatedText text={courseToShow.title} />
           </p>
           {/* watching place */}
           <div className="  !w-[1px] !h-4 bg-[#303141]"></div>
           <div className="flex items-center justify-center">
-          <p>Video</p>
+            <p>Video</p>
             <Dot width={30} height={30} />
             <p>4 min</p>
           </div>
@@ -68,7 +68,7 @@ const LearningStudyProgressCard = ({ study }: { study: Study }) => {
         {/* continue button */}
         <Button
           variant="outline"
-          className="w-[210px] border-blue-500 text-blue-500 text-md hover:bg-blue-500 hover:text-white"
+          className="lg:w-[210px] w-full border-blue-500 text-blue-500 text-md hover:bg-blue-500 hover:text-white"
         >
           Continue
         </Button>
