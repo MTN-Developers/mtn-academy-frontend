7f360e74ee80b0d73168397c287595e8f5fc8648
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2025-02-27 20:22:20 +0200

    feat: add OurDiplomas and FeedbackCarousel components to the home page

--- app/Sitecomponents/common/home/FreeStudySection.tsx
+++ app/Sitecomponents/common/home/FreeStudySection.tsx
@@ -1,51 +1,60 @@
-import StudyCard from "./StudyCard";
+'use client';
+import useGetAllSemesters from '@/app/hooks/useGetAllSemesters';
+import StudyCard from './StudyCard';
 
 export default function FreeStudySection() {
-  const studiesInfo = [
-    {
-      title: "ايقظ الفطرة",
-      image:
-        "https://efficaciousleadership.com/awaken-fittra/_next/static/media/Logo%20web.bc396bf5.png",
-      url: "https://forms.mtninstitute.net/awaken-fittra-dubai",
-      description:
-        "برنامج أيقظ الفطرة هو برنامج تدريبي، تفاعلي، مُكثف ينقلك من حال المُتفرج إلى حال الفاعلية بأساليب مدروسة ومُتقنة، يُقدمه الأستاذ ويل ستيل تحت إشراف الدكتور/أحمد الدملاوي. برنامج تُفعل فيه الفطرة، لتستلم زمام حياتك من جديد وتكون على الطريق الصحيح، حيث الفرص والإمكانيات اللامحدودة.",
-    },
-    {
-      title: "محو الامية الشعورية",
-      image:
-        "https://managethenow.com/emotional-literacy/_next/static/media/logo.f54f6b79.svg",
-      url: "https://forms.mtninstitute.net/ebt",
-      description:
-        "برنامج محو الأمية الشعورية هو رحلتك لاكتشاف العالم الخفي للمشاعر وتأثيره على حياتك اليومية. دعنا نساعدك على فهم ذاتك، تحقيق التوازن، وإدارة مشاعرك بوعي لتعيش حياة أكثر سعادة وإيجابية.",
-    },
-    {
-      title: "ممارس اختبار لوشر التشخيصي بالالوان",
-      image: "https://managethenow.com/luscher/images/logo.png",
-      url: "https://forms.mtninstitute.net/luscher",
-      description:
-        "الطبيب الوحيد في العالم العربي الذي يعتمد في تشخيص وعلاج عملائه على منظور علاجي وتدريبي ينبع من تشخيص النفس بالألوان وتشخيص المرض بالسبب النفسي له. علم الميتاهيلث منذ عام 2012 حتى الآن.",
-    },
-    {
-      title: "منطقية الشعور",
-      image:
-        "https://s3.amazonaws.com/dynamic.form/uploads/1735733175937-logical-feeling.png",
-      url: "https://forms.mtninstitute.net/logical-feeling",
-      description:
-        "نحن هنا لنساعدك على كشف أسرار جسدك وعقلك باستخدام أحدث التقنيات. يقدم مركز إدارة اللحظة خدمة التشخيص باستخدام تقنية التصوير المقطعي على المخ (CT Scan)، لتحديد أي ألم جسدي أو سلوك غير مفهوم، مما يتيح لك رؤية أوضح وفهم أعمق لذاتك.",
-    },
-  ];
+  const { data: semesters, isLoading, error } = useGetAllSemesters();
+
+  console.log('data is ', semesters);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
+  // const studiesInfo = [
+  //   {
+  //     title: 'ايقظ الفطرة',
+  //     image: 'https://efficaciousleadership.com/awaken-fittra/_next/static/media/Logo%20web.bc396bf5.png',
+  //     url: 'https://forms.mtninstitute.net/awaken-fittra-dubai',
+  //     description:
+  //       'برنامج أيقظ الفطرة هو برنامج تدريبي، تفاعلي، مُكثف ينقلك من حال المُتفرج إلى حال الفاعلية بأساليب مدروسة ومُتقنة، يُقدمه الأستاذ ويل ستيل تحت إشراف الدكتور/أحمد الدملاوي. برنامج تُفعل فيه الفطرة، لتستلم زمام حياتك من جديد وتكون على الطريق الصحيح، حيث الفرص والإمكانيات اللامحدودة.',
+  //   },
+  //   {
+  //     title: 'محو الامية الشعورية',
+  //     image: 'https://managethenow.com/emotional-literacy/_next/static/media/logo.f54f6b79.svg',
+  //     url: 'https://forms.mtninstitute.net/ebt',
+  //     description:
+  //       'برنامج محو الأمية الشعورية هو رحلتك لاكتشاف العالم الخفي للمشاعر وتأثيره على حياتك اليومية. دعنا نساعدك على فهم ذاتك، تحقيق التوازن، وإدارة مشاعرك بوعي لتعيش حياة أكثر سعادة وإيجابية.',
+  //   },
+  //   {
+  //     title: 'ممارس اختبار لوشر التشخيصي بالالوان',
+  //     image: 'https://managethenow.com/luscher/images/logo.png',
+  //     url: 'https://forms.mtninstitute.net/luscher',
+  //     description:
+  //       'الطبيب الوحيد في العالم العربي الذي يعتمد في تشخيص وعلاج عملائه على منظور علاجي وتدريبي ينبع من تشخيص النفس بالألوان وتشخيص المرض بالسبب النفسي له. علم الميتاهيلث منذ عام 2012 حتى الآن.',
+  //   },
+  //   {
+  //     title: 'منطقية الشعور',
+  //     image: 'https://s3.amazonaws.com/dynamic.form/uploads/1735733175937-logical-feeling.png',
+  //     url: 'https://forms.mtninstitute.net/logical-feeling',
+  //     description:
+  //       'نحن هنا لنساعدك على كشف أسرار جسدك وعقلك باستخدام أحدث التقنيات. يقدم مركز إدارة اللحظة خدمة التشخيص باستخدام تقنية التصوير المقطعي على المخ (CT Scan)، لتحديد أي ألم جسدي أو سلوك غير مفهوم، مما يتيح لك رؤية أوضح وفهم أعمق لذاتك.',
+  //   },
+  // ];
 
   return (
     <div className="container md:w-[80%] mx-auto mt-20">
       <div className="flex items-end justify-center md:justify-between">
-        <h1 className="text-3xl md:text-4xl font-bold text-[#353535]">
-          Free Study Section
-        </h1>
+        <h1 className="text-3xl md:text-4xl font-bold text-[#353535]">Free Study Section</h1>
         {/* <span className="hidden md:inline text-lg">See all</span> */}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-5 gap-5">
-        {studiesInfo.map((study, index) => {
-          return <StudyCard key={index} study={study} />;
+        {semesters?.map((semester, index) => {
+          return <StudyCard key={index} semester={semester} />;
         })}
       </div>
     </div>
--- app/Sitecomponents/common/home/HomeContent.tsx
+++ app/Sitecomponents/common/home/HomeContent.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import * as React from "react";
-import bg from "@/public/icons/spiral.svg";
-import { useParams } from "next/navigation";
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import * as React from 'react';
+import bg from '@/public/icons/spiral.svg';
+import { useParams } from 'next/navigation';
 
 export default function HomeContent() {
   const params = useParams();
@@ -14,27 +14,20 @@ export default function HomeContent() {
     <div className="mx-auto mt-10 md:mt-32 grid md:grid-cols-2 px-4 md:px-20">
       {/* Left Section with Background Image */}
       <div className="relative flex items-center justify-center w-full border-[#4A1D1F] border-0 md:border-0 md:border-r-2">
-        <Image
-          src={bg}
-          alt="Background Image"
-          className="hidden md:block md:absolute"
-        />
+        <Image src={bg} alt="Background Image" className="hidden md:block md:absolute" />
         {/* Pseudo-element for the border */}
         <div className="absolute bottom-0 left-0 w-[25%] h-[1px] bg-[#4A1D1F] md:hidden"></div>
-        <h1 className="text-2xl  md:text-6xl py-5 font-bold text-primary-blue">
-          Signup now for free
-        </h1>
+        <h1 className="text-2xl  md:text-6xl py-5 font-bold text-primary-blue">Signup now for free</h1>
       </div>
 
       {/* Right Section */}
       <div className="py-5 md:px-6 flex flex-col items-start justify-center">
         <p className="text-xl">
-          اختار ما يناسبك من مجموعة شاملة من البرامج المختصة بكل ما يتعلق بتحسين
-          حياتك والارتقاء بمشاعرك، لتنعكس على كل جوانب حياتك بالمزيد من الاتزان
-          والصحة النفسية والجسدية
+          اختار ما يناسبك من مجموعة شاملة من البرامج المختصة بكل ما يتعلق بتحسين حياتك والارتقاء بمشاعرك، لتنعكس على كل
+          جوانب حياتك بالمزيد من الاتزان والصحة النفسية والجسدية
         </p>
         <Button className="bg-primary-blue mt-5 border-primary-blue min-w-fit hover:bg-primary-blue">
-          {locale === "ar" ? "تسجيل الدخول" : "Join now"}
+          {locale === 'ar' ? 'تسجيل الدخول' : 'Join now'}
         </Button>
       </div>
     </div>
