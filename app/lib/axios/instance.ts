bb3309ac94ac51f7466e0c17c12a6d2b48118cdc
Mazin Islam <mazinislam431@gmail.com>
2025-02-27 11:01:56 +0200

    refactor axios instance: streamline request and response interceptors

--- app/lib/axios/instance.ts
+++ app/lib/axios/instance.ts
@@ -1,6 +1,7 @@
+'use client';
 // src/lib/axios/instance.ts
-import axios, { InternalAxiosRequestConfig } from "axios";
-import { deleteCookie, getCookie, setCookie } from "cookies-next";
+import axios, { InternalAxiosRequestConfig } from 'axios';
+import { deleteCookie, getCookie, setCookie } from 'cookies-next';
 // import { cookies } from "next/headers";
 
 const axiosInstance = axios.create({
@@ -9,51 +10,41 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   async function (config: InternalAxiosRequestConfig) {
-    if (typeof window === "undefined") {
-      // const access_token = cookies().get("access_token");
-      // config.headers["Authorization"] = `Bearer ${access_token}`;
-      return config;
-    } else {
-      if (config.headers)
-        config.headers["Authorization"] = `Bearer ${getCookie("access_token")}`;
-      return config;
-    }
+    if (config.headers) config.headers['Authorization'] = `Bearer ${getCookie('access_token')}`;
+    return config;
   },
   function (error: unknown) {
     return Promise.reject(error);
-  }
+  },
 );
 
 axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
+  response => response,
+  async error => {
     const originalRequest = error.config;
     if (error?.response?.status === 401) {
       // Refresh Token is Expired
-      if (
-        error?.response?.data?.requiresLogin === true &&
-        window.location.pathname !== "/ar/videos-list"
-      ) {
-        deleteCookie("access_token");
-        deleteCookie("refresh_token");
-        deleteCookie("user");
-        console.log("Redirecting to login page 2");
-        window.location.href = "/login";
-        return Promise.reject(new Error("Requires login"));
+      if (error?.response?.data?.requiresLogin === true) {
+        deleteCookie('access_token');
+        deleteCookie('refresh_token');
+        deleteCookie('user');
+        console.log('Redirecting to login page 2');
+        window.location.href = '/login';
+        return Promise.reject(new Error('Requires login'));
       } else {
         // Refresh Token Not Expired
-        const refreshToken = getCookie("refresh_token");
-        const response = await axiosInstance.post("/auth/refresh", {
+        const refreshToken = getCookie('refresh_token');
+        const response = await axiosInstance.post('/auth/refresh', {
           refresh_token: refreshToken,
         });
         const { access_token } = response.data.data;
-        setCookie("access_token", access_token);
-        originalRequest.headers["Authorization"] = `Bearer ${access_token}`;
+        setCookie('access_token', access_token);
+        originalRequest.headers['Authorization'] = `Bearer ${access_token}`;
         return axiosInstance(originalRequest);
       }
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default axiosInstance;
