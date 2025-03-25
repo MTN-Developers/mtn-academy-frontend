835343962769e4af67c5015bf362a91a92f3e90c
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2025-03-25 14:30:19 +0200

    fix: remove 'Connection' header setting in middleware response

--- middleware.ts
+++ middleware.ts
@@ -52,9 +52,6 @@ export function middleware(req: NextRequest) {
     const redirectURL = `/${locale}/login?redirect=${pathnameWithoutLocale}`;
     const response = NextResponse.redirect(new URL(redirectURL, req.url));
 
-    //
-    response.headers.set('Connection', 'close');
-
     // Clear any existing auth cookies
     response.cookies.delete('accessToken');
     response.cookies.delete('refreshToken');
