887cc202305ff940767d4aba9270ff6609b8689e
Ahmed Esmail <ahmedp30895533@pharm.tanta.edu.eg>
2025-05-02 15:40:54 +0300

    Add StandaloneStudy component and related functionality for fetching free studies
    - Introduced StandaloneStudy component to display free studies.
    - Implemented useGetAllFreeStudies hook for data fetching.
    - Created StandaloneStudyCard component for individual study display.
    - Updated FreeStudiesComp to integrate new components.
    - Added new types for free study data in freeStudy.d.ts.
    - Configured next.config.mjs to allow images from new domain.

---
+++ app/components/standaloneStudy/StandaloneStudy.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import useGetAllFreeStudies from '@/app/hooks/useGetAllFreeStudies';
+import { FreeStudyCourse } from '@/app/types/freeStudy';
+import { useParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import StandaloneStudyCard from './StandaloneStudyCard';
+
+const StandaloneStudy = () => {
+  const param = useParams();
+  const locale = param.locale as string;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [allStudies, setAllStudies] = useState<FreeStudyCourse[]>([]);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const { data, error, isError, isLoading, isFetching } = useGetAllFreeStudies({
+    limit: 4,
+    page: currentPage,
+  });
+
+  useEffect(() => {
+    if (data?.data.meta) {
+      setTotalPages(data.data.meta.totalPages);
+    }
+  }, [data?.data.meta]);
+
+  useEffect(() => {
+    if (data?.data.data) {
+      setAllStudies(prev => [...prev, ...data.data.data]);
+    }
+  }, [data?.data.data]);
+
+  if (isLoading && currentPage === 1) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    console.log(error);
+    return <>Error...</>;
+  }
+
+  const hasMore = currentPage < totalPages;
+
+  return (
+    <>
+      {allStudies.length > 0 && (
+        <div className="lg:my-10 my-4 p-4">
+          <h2 className="text-[#353535] lg:text-[40px] my-4 font-semibold leading-[45px]">
+            {locale === 'ar' ? 'برامج متخصصة' : 'Free Studies'}
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
+            {allStudies.map(study => (
+              <StandaloneStudyCard key={study.id} study={study} />
+            ))}
+          </div>
+
+          {hasMore && (
+            <button
+              onClick={() => setCurrentPage(prev => prev + 1)}
+              disabled={isFetching}
+              className="mt-4 text-blue-600 hover:text-blue-800 disabled:opacity-50"
+            >
+              {isFetching ? 'Loading...' : 'See More'}
+            </button>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default StandaloneStudy;
---
+++ app/components/standaloneStudy/StandaloneStudyCard.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { FreeStudyCourse } from '@/app/types/freeStudy';
+import Image from 'next/image';
+import { useParams } from 'next/navigation';
+import React from 'react';
+
+type Props = {
+  study: FreeStudyCourse;
+};
+
+const StandaloneStudyCard = ({ study }: Props) => {
+  const param = useParams();
+  const locale = param.locale as string;
+  return (
+    <div
+      key={study.id}
+      className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
+    >
+      {study.banner_ar && study.banner_en && (
+        <a href="#">
+          <Image className="rounded-t-lg" src={locale === 'ar' ? study.banner_ar : study.banner_en} alt="" />
+        </a>
+      )}
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {locale === 'ar' ? study.name_ar : study.name_en}
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {locale === 'ar' ? study.description_ar : study.description_en}
+        </p>
+        <a
+          href="#"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Read more
+          <svg
+            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default StandaloneStudyCard;
